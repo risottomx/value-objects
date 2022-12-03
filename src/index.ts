d9f867b482b231bb0d563e3e10ec0627ea10030a
PepeFlores97 <pepefloressalas@gmail.com>
2022-12-03 17:54:10 -0600

    feat(core): export all Value Objects

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { ValueObject } from './value-object';
+import { Uuid } from './uuid';
+import { StringValueObject } from './string';
+import { NumberValueObject } from './number';
+import { EnumValueObject } from './enum';
+
+export {
+  ValueObject,
+  Uuid,
+  StringValueObject,
+  NumberValueObject,
+  EnumValueObject,
+};

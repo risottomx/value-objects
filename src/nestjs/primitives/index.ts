6fe2bf1ec8bbf228c5a823a27abd129b354fd8e7
PepeFlores97 <pepefloressalas@gmail.com>
2022-12-16 13:31:29 -0600

    feat(primitives): add support to nestjs AggregateRoot

---
+++ src/nestjs/primitives/index.ts
@@ -0,0 +1,8 @@
+/**
+ * This Primitive type is only for nestjs use
+ * @nestjs/cqrs library has prperties on AggregateRoot
+ * class that we do not need in our Primitive type
+ */
+
+import { Primitives } from './primitives';
+export { Primitives };
